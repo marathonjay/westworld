f438da725f2acc9c12b637be135d9b0e715a2cae
Jason Parr <parr.jason.01@gmail.com>
2020-12-28 11:43:31 -0800

    added variables to the js sheet for the music files.

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -21,23 +21,41 @@ for (i = 0; i < cardClick.length; i++) {
 
 //////* AUDIO FUNCTIONS *////
 
-var audio = new Audio("assets/audio/westworld-main-theme.mp3");
+const westworldTheme = new Audio("assets/audio/westworld-main-theme.mp3");
+const blackHoleSun = new Audio("assets/audio/black-hole-sun.mp3");
+const paintItBlack = new Audio("assets/audio/paint-it-black.mp3");
+const heartShapedBox = new Audio("assets/audio/heart-shaped-box.mp3");
+const spaceOddity = new Audio("assets/audio/space-oddity.mp3");
+const houseOfTheRisingSun = new Audio("assets/house-of-the-rising-sun.mp3");
+const nitroHeist = new Audio("assets/audio/nitro-heist.mp3");
+const sweetChildOMine = new Audio("assets/audio/sweet-child-o-mine.mp3");
+
+
+
+
 
 document.querySelector(".audioPlay").addEventListener("click", function() {
-  audio.play();
+  westworldTheme.play();
 });
   // console.log(audio.currentTime);
 
 document.querySelector(".audioPause").addEventListener("click", function() {
-  audio.pause();
+  westworldTheme.pause();
   // console.log(audio.currentTime);
 } );
 
 document.querySelector(".audioStop").addEventListener("click", function() {
-  audio.pause();
-  audio.currentTime = 0;
+  westworldTheme.pause();
+  westworldTheme.currentTime = 0;
   // console.log(audio.currentTime);
 } );
+
+
+
+
+
+
+
 
 
 
